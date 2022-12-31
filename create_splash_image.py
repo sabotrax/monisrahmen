81f1794eec70322cee90f1fd172b1d7624dc5681
Marcus Schommer <marcus@dankesuper.de>
2022-12-31 19:57:03 +0100

    - changed the network probing for the info/startup screen from passive   to active.

--- create_splash_image.py
+++ create_splash_image.py
@@ -2,16 +2,28 @@
 
 import config
 import netifaces as ni
+import requests
+import time
 from PIL import Image, ImageDraw, ImageFont
 
 image_text = f'E-Mail: {config.email_user}\n\n'
 
-try:
+connected = False
+
+for i in range(5):
+    try:
+        req = requests.get("https://google.com", timeout=5)
+        connected = True
+        break
+    except (requests.ConnectionError, requests.Timeout):
+        time.sleep(5)
+        pass
+
+if connected:
     ip_address = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
     # this path should match with the Samba configuration
     image_text += f'Freigabe: \\\\{ip_address}\\bilder'
-except Exception as e:
-    print(e)
+else:
     image_text += config.network_error
 
 # switched because of portrait mode
