ae231dd2a32a935c9eb91452ec2c34608065f09b
Marcus Schommer <marcus@dankesuper.de>
2023-01-01 12:25:48 +0100

    - changed the check for duplicate pictures.   until now get_pics_by_mail.py checked if the file name   of the attachment was already existing and skipped the   processing of the mail if it was.   I thought this was reasonable because photo apps normally   do choose unique file names since they often encode meta   data in them. But it's not like that necessarily.   So, now a checksum of the picture is created and compared   to the existing images.   A random file name is created for the image before saving it.   File name, email sender, date and checksum are written to   TinyDB. - updated requirements.txt because of tinydb. - removed obsolete code create_filename().

--- get_pics_by_mail.py
+++ get_pics_by_mail.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
-# import base64
 import config
 import email
+import hashlib
 import imaplib
 import os
 import random
 import re
 import string
-from datetime import datetime
+from email.header import decode_header
 from PIL import Image
+from time import time
+from tinydb import TinyDB, Query
+
+db = TinyDB(config.project_path + '/user_data/db.json')
 
 imap = imaplib.IMAP4_SSL(config.email_host, config.email_port)
 imap.login(config.email_user, config.email_pass)
@@ -39,37 +43,36 @@ def process_image(filePath):
     print("image rotated")
 
 
-# create a useful file name
-# currently unused
-def create_filename(email_message):
-    match = re.search(r'From:\s.*<(\S+@\S+)>\n', str(email_message))
-    print(match)
-    if match:
-        filename = re.sub(r'[^\w]', '_', match.group(1))
-        now = datetime.now()
-        filename += now.strftime("-%Y%m%d-%H%M-")
-        letters = string.ascii_lowercase
-        rnd_str = ''.join(random.choice(letters) for i in range(5))
-        filename += rnd_str
-        return filename
-
-
 i = 0
 for num in id_list:
-    if i >= 3:
+    if i >= 10:
         break
     typ, data = imap.fetch(num, '(RFC822)')
+
+    msg = email.message_from_bytes(data[0][1])
+    # decode the email subject
+    email_subject, encoding = decode_header(msg["Subject"])[0]
+    if isinstance(email_subject, bytes):
+        # if it's a bytes, decode to str
+        email_subject = email_subject.decode(encoding)
+    print("Subject:", email_subject)
+    # check the email subject for the keyword
+    if not re.search(rf'{config.email_keyword}', email_subject, re.IGNORECASE):
+        print("skip!")
+        i += 1
+        continue
+
+    # decode email sender
+    email_sender, encoding = decode_header(msg.get("From"))[0]
+    if isinstance(email_sender, bytes):
+        email_sender = email_sender.decode(encoding)
+    print("From:", email_sender)
+
     # converts byte literal to string removing b''
     raw_email = data[0][1]
     raw_email_string = raw_email.decode('utf-8')
     email_message = email.message_from_string(raw_email_string)
-    # check the email subject for the keyword
-    match = re.search(r'Subject:\s([\w\s]+)\n', str(email_message), re.DOTALL)
-    print(match)
-    if not match or match.group(1).lower() != config.email_keyword:
-        print("skip!")
-        i += 1
-        continue
+
     # download attachments
     for part in email_message.walk():
         if part.get_content_maintype() == 'multipart':
@@ -78,26 +81,54 @@ for num in id_list:
             continue
         fileName = part.get_filename()
         if bool(fileName):
-            filePath = os.path.join(config.picture_path, fileName)
-            # skip if the file exists
-            if not os.path.isfile(filePath):
-                fp = open(filePath, 'wb')
-                fp.write(part.get_payload(decode=True))
-                fp.close()
-                print("attachment: ", filePath)
-                try:
-                    # verify image
-                    img = Image.open(filePath)
-                    img.verify()
-                    img.close()
-                    print("image verified")
-                except Exception as e:
-                    print(e)
-                    os.remove(filePath)
-                    i += 1
-                    continue
+            # create image checksum
+            raw = part.get_payload(decode=True)
+            hashed_raw = hashlib.sha1()
+            hashed_raw.update(raw)
+            hd = hashed_raw.hexdigest()
+            print("digest: ", hd)
 
-                # process_image(filePath)
+            # look up checksum
+            picture = Query()
+            duplicate = db.search(picture.checksum == hd)
+            # skip duplicates
+            if duplicate:
+                print("duplicate -> skip!")
+                i += 1
+                continue
+            else:
+                print("unique copy")
+
+            # create unique file name
+            letters = string.ascii_lowercase
+            rnd_str = ''.join(random.choice(letters) for i in range(7))
+            fileName = f'{rnd_str}-{fileName}'
+            print("new file name: ", fileName)
+            # skip if the file exists
+            # if not os.path.isfile(filePath):
+
+            filePath = os.path.join(config.picture_path, fileName)
+            fp = open(filePath, 'wb')
+            fp.write(part.get_payload(decode=True))
+            fp.close()
+            print("attachment: ", filePath)
+            try:
+                # verify image
+                img = Image.open(filePath)
+                img.verify()
+                img.close()
+                print("image verified")
+
+                # write properties to db
+                db.insert({'filename': fileName, 'sender': email_sender,
+                           'date': int(time()), 'checksum': hd})
+
+            except Exception as e:
+                print(e)
+                os.remove(filePath)
+                i += 1
+                continue
+
     i += 1
     if config.delete_email:
         print("deleted email: ", num)
