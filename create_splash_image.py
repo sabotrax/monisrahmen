de917ebd8edced6ceed5890326f835d00439a91e
Marcus Schommer <marcus@dankesuper.de>
2023-01-04 21:39:45 +0100

    - replaced config.py with .env for configuration options   in create_splash_image.py. - made network interface configurable.

--- create_splash_image.py
+++ create_splash_image.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-import config
 import netifaces as ni
 import requests
 import time
+from decouple import config
 from PIL import Image, ImageDraw, ImageFont
 
-image_text = f'E-Mail: {config.email_user}\n\n'
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+image_text = f"E-Mail: {config('EMAIL_USER')}\n\n"
 
 connected = False
 
@@ -20,19 +22,19 @@ for i in range(11):
         pass
 
 if connected:
-    ip_address = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
+    ip_address = ni.ifaddresses(config('NETWORK_DEVICE'))[ni.AF_INET][0]['addr']
     # this path should match with the Samba configuration
     image_text += f'Freigabe: \\\\{ip_address}\\bilder'
 else:
-    image_text += config.network_error
+    image_text += config('NETWORK_ERROR')
 
 # switched because of portrait mode
-width = config.screen_height
-height = config.screen_width
+width = config('SCREEN_HEIGHT', cast=int)
+height = config('SCREEN_WIDTH', cast=int)
 
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",
-                          size=config.font_size)
+                          size=config('FONT_SIZE', cast=int))
 img = Image.new('RGB', (width, height), color='black')
 imgDraw = ImageDraw.Draw(img)
 imgDraw.text((10, 10), image_text, font=font, fill=(119, 136, 153))
-img.save(config.project_path + '/site_data/splash.png')
+img.save(config('PROJECT_PATH') + '/site_data/splash.png')
