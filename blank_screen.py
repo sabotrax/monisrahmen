26a1c3448cae63f6dedb954c9f93e8c6b72f03b1
Marcus Schommer <marcus@dankesuper.de>
2023-01-05 20:07:06 +0100

    - replaced config.py with .env for configuration options   in blank_screen.py. - added code to use the radar sensor for room monitoring.

--- blank_screen.py
+++ blank_screen.py
@@ -4,10 +4,16 @@
 # see README.md
 
 import atexit
-import config
 import RPi.GPIO as GPIO
+import smtplib
+import ssl
 import subprocess
+from datetime import datetime
+from decouple import config
+from email.message import EmailMessage
 from time import sleep, time
+
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 GPIO.setmode(GPIO.BCM)
 
@@ -21,7 +27,6 @@ GPIO.output(27, GPIO.HIGH)
 # display brightness pwm
 GPIO.setup(18, GPIO.OUT)
 dim = GPIO.PWM(18, 1000)
-
 
 @atexit.register
 def goodbye():
@@ -50,23 +55,91 @@ def dim_display(mode):
         sleep(0.5)
 
 
+def is_monitoring():
+    monitor = False
+
+    try:
+        from_hour, from_minute = config('START_MONITORING', default="").split(":")
+        to_hour, to_minute = config('END_MONITORING', default="").split(":")
+    except ValueError:
+        return monitor
+
+    try:
+        from_hour = abs(int(from_hour))
+        to_hour = abs(int(to_hour))
+        from_minute = abs(int(from_minute))
+        to_minute = abs(int(to_minute))
+    except Exception as e:
+        print(e)
+        return monitor
+
+    now = datetime.now()
+    this_hour = now.hour
+    this_minute = now.minute
+    print(f'now: {this_hour}:{this_minute}')
+
+    if from_hour == 24:
+        from_hour = 0
+    if (from_hour != to_hour) and to_minute == 0:
+        to_minute = 60
+    if from_hour <= to_hour:
+        if from_hour < this_hour < to_hour:
+            monitor = True
+        elif from_hour == this_hour:
+            if from_minute <= this_minute < to_minute:
+                monitor = True
+    else:
+        if from_hour <= this_hour < 24 or 0 <= this_hour < to_hour:
+            monitor = True
+        elif from_hour == this_hour:
+            if from_minute <= this_minute < to_minute:
+                monitor = True
+
+    return monitor
+
+
+def send_mail():
+    msg = EmailMessage()
+    msg.set_content("The body of the email is here")
+    msg["Subject"] = "An Email Alert"
+    msg["From"] = "from@example.com"
+    msg["To"] = "to@example.com"
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP("mail.example.com", port=587) as smtp:
+        smtp.starttls(context=context)
+        smtp.login("from@example.com", "secretpassword")
+        smtp.send_message(msg)
+
+
 if __name__ == "__main__":
     blanked = False
     active = idle = time()
+    send_alert = False
     while True:
         if motion_detect():
-            # print("motion detected")
+            if DEBUG:
+                print("motion detected")
             active = time()
             if blanked:
                 subprocess.run(["/usr/bin/vcgencmd", "display_power",
                                 "1", "2"])
                 dim_display("on")
                 blanked = False
+                #now = datetime.now()
+                #if not send_alert and (start_monitoring <= now <=
+                                       #end_monitoring):
+                    #print("send mail")
+                    #send_mail()
+                    #send_alert = True
         else:
-            # print("not much")
+            if DEBUG:
+                print("not much")
             idle = time()
-            if not blanked and (idle - active > config.display_timeout):
-                # print("blank now")
+            if not blanked and (idle - active > config('DISPLAY_TIMEOUT', cast=int)):
+                if DEBUG:
+                    print("blank now")
                 dim_display("off")
                 subprocess.run(["/usr/bin/vcgencmd", "display_power",
                                 "0", "2"])
